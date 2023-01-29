c71ffaf4ed2c9db61c30a715f65b30757bf5117c
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-01-29 10:02:14 +0000

    adding ajax to get lon and lat from response and put into second URL using second ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var APIKey = "5ff58d1744fba6ecdad3c248282449e9";
 
 
 var cardTodayName = document.getElementById('cardTodayName');
-var cardTodayDate = document.getElementById('cardTodayDate')
+var cardTodayDate = document.getElementById('cardTodayDate');
+var todayIcon = document.getElementById('todayIcon');
 var cardTodayBody = document.getElementById('cardTodayBody');
 
 
@@ -21,36 +22,90 @@ var cityHistory = [];
 var userCity;
 var city;
 
-$('#search-button').on('click', function (e) {
-    // set user's input to 'city' for url
-    var userCity = $('#search-input').val();
-    //alert($('#search-input').val());
-    //variable to store url
-    e.preventDefault();
-    var city = userCity;
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
-    console.log(queryURL);
-    
-    console.log(city);
-    
-    // function to add data to today card
-function weatherToday() {
-    $.ajax({
-       url: queryURL,
-       method: 'GET'
-   }).then(function (response) {
-       cardTodayName.textContent = response.name;
-       cardTodayDate.textContent = currentDate;
-       temperature = response.main.temp;
-       windSpeed = response.wind.speed;
-       humidity = response.main.humidity;
 
-       alert('Temperature: ' + temperature);
-       alert('Wind Speed: ' + windSpeed);
-       alert('Humidity: ' + humidity);
-   })
-   
-}
-weatherToday()
+// on click of search button
+$('#search-button').on('click', function (e) {
+    
+    //prevent default behaviour
+    e.preventDefault();
+    //set user's input to 'city' for url
+    var userCity = $('#search-input').val();
+    // variable to store url
+    var city = userCity;
+    var cityURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + 1 + "&appid=" + APIKey;
+
+    //function to get lon and lat
+    function getLongLat() {
+        $.ajax({
+            url: cityURL,
+            method: "GET"
+        }).then(function (lonLatResponse) {
+            var lon = lonLatResponse[0].lon;
+            var lat = lonLatResponse[0].lat;
+            var cityName = lonLatResponse[0].name;
+
+            var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+
+            $.ajax({
+                url: weatherURL,
+                method: "GET"
+            }).then(function (weatherResponse) {
+                cardTodayName.textContent = cityName;
+                cardTodayDate.textContent = currentDate;
+                var iconValue = weatherResponse.list[0].weather[0].icon;
+                var imgURL = "http://openweathermap.org/img/wn/" + iconValue + ".png";
+                var iconImg = $('<img>').attr('src', imgURL);
+                $('#imgContainer').append(iconImg);
+                console.log(weatherURL);
+            })
+        })
+    }
+
+    getLongLat();
 
 });
+
+
+
+// $('#search-button').on('click', function (e) {
+//     // set user's input to 'city' for url
+//     var userCity = $('#search-input').val();
+//     //alert($('#search-input').val());
+//     //variable to store url
+//     e.preventDefault();
+//     var city = userCity;
+//     var cityURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+//     console.log(cityURL);
+    
+//     console.log(city);
+    
+//     // function to add data to today card
+// function weatherToday() {
+//     $.ajax({
+//        url: queryURL,
+//        method: 'GET'
+//    }).then(function (response) {
+        
+
+//        cardTodayName.textContent = response.name;
+//        cardTodayDate.textContent = currentDate;
+//        temperature = response.main.temp;
+//        windSpeed = response.wind.speed;
+//        humidity = response.main.humidity;
+
+//        alert('Temperature: ' + temperature);
+//        alert('Wind Speed: ' + windSpeed);
+//        alert('Humidity: ' + humidity);
+//    })
+   
+// }
+// weatherToday()
+
+// });
+
+
+// var city = userCity;
+// var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + 1 + "&appid=" + APIKey;
+
+
+
